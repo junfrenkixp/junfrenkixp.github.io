1d6ebcff0e980eff538bf8893c75651763850f0e
Artsion Zavaley <azavaley@bk.ru>
2019-09-16 01:35:02 +0300

    Bug fixed (mobile_menu.js).

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -12,33 +12,6 @@
   const navMenuLangsLinks = document.querySelectorAll(".nav_menu-links_lang-links");
   const anchorLinks = [navMenuLangsLinks, navMenuAnchorsLinks, logoTwoLetters];
 
-  function sticky() {
-    const scrolled = window.pageYOffset;
-    if (scrolled > 1) {
-      navMenu.classList.add("nav_menu-sticky");
-      for (let i = 0; i < anchorLinks.length; i++) {
-        for (let j = 0; j < anchorLinks[i].length; j++) {
-          anchorLinks[i][j].classList.add("mobile_menu-links--active");
-        }
-      }
-      for (let i = 0; i < burgerMenuButtonElements.length; i++) {
-        burgerMenuButtonElements[i].classList.add("mobile_menu-links--active-bg");
-      }
-    }
-    if (scrolled < 1) {
-      navMenu.classList.remove("nav_menu-sticky");
-      for (let i = 0; i < anchorLinks.length; i++) {
-        for (let j = 0; j < anchorLinks[i].length; j++) {
-          anchorLinks[i][j].classList.remove("mobile_menu-links--active");
-        }
-      }
-      for (let i = 0; i < burgerMenuButtonElements.length; i++) {
-        burgerMenuButtonElements[i].classList.remove("mobile_menu-links--active-bg");
-      }
-    }
-  }
-  window.addEventListener("scroll", sticky);
-  //
   burgerMenuButton.addEventListener("click", function() {
     navMenuLinks.classList.toggle("mobile_menu--active");
     if (burgerMenuButtonElements[0].classList.contains("mobile_menu-links--active-bg_child")) {
