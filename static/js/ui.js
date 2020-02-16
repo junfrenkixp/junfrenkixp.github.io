53b9171f2ce1f79ddcab79d73a9abc71620ba7a1
junfrenkixp <penin13579@gmail.com>
2020-02-16 03:16:09 +0300

    parallax removed; add smooth scroll and sticky menu(rewrite code in the future PLEASE ARTSIOM)

--- static/js/ui.js
+++ static/js/ui.js
@@ -18,13 +18,41 @@
 	}
 	mobileMenu();
 
-	function headerParallax() {
-		window.addEventListener("scroll", function() {
-			if (window.scrollY < 700) {
-				const item = document.querySelector("header");
-				item.style.transform = "translateY(" + window.scrollY * 0.5 + "px)";
-			}
-		});
+	function stickyMenu() {
+		let scrolled = window.pageYOffset;
+		const navContainer = document.querySelector(".nav-container");
+		if (scrolled > 300) {
+			navContainer.classList.add("sticky");
+		}
+		if (scrolled < 300) {
+			navContainer.classList.remove("sticky");
+		}
 	}
-	headerParallax();
+	window.addEventListener("scroll", stickyMenu);
+
+	const navbarLinks = document.querySelectorAll(".nav-menu a");
+	for (let i = 0; i < navbarLinks.length; i++) {
+		navbarLinks[i].addEventListener("click", smoothScroll);
+	}
+	function smoothScroll(event) {
+		const targetId = event.currentTarget.getAttribute("href") === "#" ? "header" : event.currentTarget.getAttribute("href");
+		const targetPosition = document.querySelector(targetId).offsetTop;
+		const startPosition = window.pageYOffset;
+		const distance = targetPosition - 100 - startPosition;
+		const duration = 1000;
+		let start = null;
+		window.requestAnimationFrame(step);
+		function step(timestamp) {
+			if (!start) start = timestamp;
+			const progress = timestamp - start;
+			window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+			if (progress < duration) window.requestAnimationFrame(step);
+		}
+	}
+	function easeInOutCubic(t, b, c, d) {
+		t /= d / 2;
+		if (t < 1) return (c / 2) * t * t * t + b;
+		t -= 2;
+		return (c / 2) * (t * t * t + 2) + b;
+	}
 })();
